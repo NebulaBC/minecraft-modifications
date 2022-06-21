b6d6f66a442f563bdde26155f5a5d5e7b6398f2f
NebulaBC <contact@nebulabc.net>
2022-06-21 15:14:12 -0400

    GitSumBackups - Register command

--- GitSumBackups/src/main/java/net/nebulabc/gitsumbackups/GitSumBackups.java
+++ GitSumBackups/src/main/java/net/nebulabc/gitsumbackups/GitSumBackups.java
@@ -1,12 +1,15 @@
 package net.nebulabc.gitsumbackups;
 
+import net.nebulabc.gitsumbackups.impl.BackupCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class GitSumBackups extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Objects.requireNonNull(this.getCommand("gitsave")).setExecutor(new BackupCommand());
 
     }
 
